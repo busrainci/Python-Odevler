a66c72a628a73f1e658b807a565d50f78b4b9ae8
busrainci <inci_busra@hotmail.com>
2023-03-22 15:28:24 +0300

    3.Ders Ödevi

---
+++ Odev3.py
@@ -0,0 +1,69 @@
+
+print("HOMEWORK 3")
+
+# HTML = 
+# İngilizce de Hayper Text Makeup Language'in kısaltmasıdır. Türkçe d eise metin işaretleme dili olarak tanımlanır. 
+# Web sayfalarını oluşturmak için kullanılan standart dildir.HTML bir programlaa dili değildir,Chrome-Yandex gibi tarayıcıların okuyup
+# anlamlandırdığı bir dildir. Bir web sayfası HTML kodlardan oluşur ve tarayıcılar tarafından bu kodlar yorumlanarak görsel bir sayfa haline getirilir.
+# HTML, web geliştiricileri tarafından web sayfalarının tasarım ve düzenlemesi için kullanılan temel bir bileşendir ve diğer web teknolojileriyle birlikte
+# kullanılarak dinamik ve etkileşimli web sayfaları oluşturulabilir..
+# JavaScript ve CSS ile beraber kullanıldığında HTML ile dinamik web siteleri oluşturulabilir.
+
+# <!DOCTYPE html>: Tüm HTML belgeleri bu kod ile başlamak zorundadır. Bu kod aslında sayfanın HTML5 formatına göre kodlandığını gösterir.
+
+# <html>: Kök etiket olarak da bilinen bu temel kullanımda tüm HTML kodları, bu iki etiket arasında tanımlanmak zorundadır.
+
+# <body>: Görünebilir tüm HTML kod parçalarının eklendiği etiket alanıdır.
+
+# <head>: Web sayfasının dili, başlığı ve diğer düzenlemelerinin yer aldığı etikettir.
+
+# <title>: Web sitesinin başlık alanını tanımlamak için kullanılır.
+
+
+# HTML Locators(Konumlandırıcı) = 
+# Web sayfalarında bulunan HTML öğelerine erişmek için kullanılan yol göstericilerdir. Test otomasyon araçları gibi araçlar,
+# web sayfalarındaki öğeleri tanımlamak ve etkileşim kurmak için HTML locators kullanırlar.HTML locators, HTML öğelerinin benzersiz bir şekilde tanımlanmasını
+# sağlar. Bu locators, HTML öğelerinin özelliklerini kullanarak tanımlanabilir. 
+
+# Örneğin:
+
+# ID: Bir öğenin benzersiz bir kimliği olabilir. Bu kimlik, "id" özniteliği ile tanımlanabilir. ID locator'ü, belirli bir öğeyi tanımlamak için kullanılabilir.
+
+# Name: Öğelerin isimleri, "name" özniteliği ile tanımlanabilir. Name locator'ü, belirli bir öğeyi tanımlamak için kullanılabilir.
+
+# Class Name: Öğelerin sınıfları, "class" özniteliği ile tanımlanabilir. Class Name locator'ü, belirli bir sınıfa sahip tüm öğeleri tanımlamak için
+# kullanılabilir.
+
+# Tag Name: Öğelerin etiketleri, "tag name" ile tanımlanabilir. Tag Name locator'ü, belirli bir etikete sahip tüm öğeleri tanımlamak için kullanılabilir.
+
+# Link Text: Bir bağlantının metni, "link text" ile tanımlanabilir. Link Text locator'ü, belirli bir bağlantıyı tanımlamak için kullanılabilir.
+
+# Partial Link Text: Bir bağlantının kısmi metni, "partial link text" ile tanımlanabilir. Partial Link Text locator'ü, belirli bir bağlantının bir bölümünü 
+# tanımlamak için kullanılabilir.
+
+# Bu locator'ler, web sayfalarında bulunan öğeleri belirleyebilir ve test otomasyonu gibi araçlarla etkileşim kurulabilmesini sağlar. Web geliştiricileri,
+# web sayfalarında belirli bir öğeye erişmek için bu locator'leri kullanarak öğeleri tanımlayabilirler.
+
+
+
+# SELENİUM'DA AKSİYONLAR =
+
+# Selenium, web uygulamalarını test etmek için kullanılan açık kaynaklı bir otomasyon aracıdır. Selenium ile test senaryoları oluştururken, 
+# kullanıcının web sayfasında gerçekleştirdiği aksiyonları taklit etmek için "aksiyonlar" kullanılır. Bazı temel aksiyonlar şunlardır:
+
+# 1-Click: Belirtilen öğeyi tıklar. Örneğin, bir buton veya bağlantıyı tıklamak için kullanılır.
+
+# 2-Send Keys: Belirtilen öğe için klavye girdisi sağlar. Örneğin, bir metin kutusuna bir metin girerken veya klavyeden bir tuşa basarken kullanılır.
+
+# 3-Clear: Belirtilen öğenin içeriğini siler. Örneğin, bir metin kutusunun içeriğini silmek için kullanılır.
+
+# 4-Get Text: Belirtilen öğenin metin içeriğini alır. Örneğin, bir bağlantının metnini okumak için kullanılır.
+
+# 5-Select: Belirtilen öğeyi seçer. Örneğin, bir dropdown listesinde bir seçenek seçmek için kullanılır.
+
+# 6-Navigate: Belirtilen URL'ye gider. Örneğin, bir web sayfasına gitmek için kullanılır.
+
+# 7-Switch To: Belirtilen öğe veya pencereye geçer. Örneğin, bir iframe'e geçmek veya bir yeni pencereye geçmek için kullanılır.
+
+# Bu aksiyonlar, Selenium ile web sayfaları üzerinde farklı test senaryoları oluşturmak için kullanılabilir. Web sayfasında bir kullanıcının gerçekleştirdiği
+# aksiyonları taklit etmek, web uygulamalarının test edilmesinde önemli bir rol oynar ve Selenium'un popüler bir otomasyon aracı olmasını sağlar.
